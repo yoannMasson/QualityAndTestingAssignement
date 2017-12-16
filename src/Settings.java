3aad90e77b25d494f1bd01bd66b579d71c62ca91
yoannMasson <yoann.masson95@gmail.com>
2017-12-16 22:14:19 +0000

    Ajout des settings par defaut

--- src/Settings.java
+++ src/Settings.java
@@ -9,7 +9,7 @@ public class Settings {
 	private String filePath;
 	private Map<String,Float> settings;
 
-	public Settings(String path) throws IOException{
+	public Settings(String path) throws Exception{
 
 		this.filePath = path;
 		this.settings = new HashMap<>();
@@ -25,14 +25,25 @@ public class Settings {
 		String[] array;
 		//Read File Line By Line
 		while ((strLine = br.readLine()) != null)   {
-			
+
 			array = strLine.split(";");
-			System.out.println(array.length);
 			if(array.length == 3 ||array.length == 2) {//The line is correct
 				settings.put(array[0],Float.parseFloat(array[1]));
 			}
 		}
 		br.close();
+		//Checking that we have at least 128 cores
+		if(settings.get("NODES_16_PROCESSOR_NUMBER")+
+				settings.get("NODES_32_PROCESSOR_NUMBER")+
+				settings.get("NODES_64_PROCESSOR_NUMBER")+
+				settings.get("ACCELERATED_16_PROCESSOR_NUMBER")+
+				settings.get("ACCELERATED_32_PROCESSOR_NUMBER")+
+				settings.get("ACCELERATED_64_PROCESSOR_NUMBER")+
+				settings.get("NODES_16_PROCESSOR_NUMBER")+
+				settings.get("SPECIALIZED_NUMBER") < 128) {
+			throw new Exception("There must be at least 128 cores in total");
+		}
+		
 	}
 
 	/**
@@ -57,7 +68,7 @@ public class Settings {
 	public Map<String,Float> getSettings() {
 		return settings;
 	}	
-	
+
 	/**
 	 * Generate the default settings
 	 * @return the settings
@@ -65,6 +76,41 @@ public class Settings {
 	public Map<String,Float> defaultSettings(){
 		this.settings = new HashMap<>();
 		this.settings.put("STUDENT_NUMBER", 60f);
+		this.settings.put("RESEARCHER_NUMBER", 20f);
+		this.settings.put("COMPUTER_BUDGET", 20f);
+		this.settings.put("WATER_BUDGET", 10f);
+		this.settings.put("SOIL_BUDGET", 5f);
+		this.settings.put("NODES_16_PROCESSOR_NUMBER", 80f);
+		this.settings.put("NODES_32_PROCESSOR_NUMBER", 40f);
+		this.settings.put("NODES_64_PROCESSOR_NUMBER", 30f);
+		this.settings.put("ACCELERATED_16_PROCESSOR_NUMBER", 30f);
+		this.settings.put("ACCELERATED_32_PROCESSOR_NUMBER", 20f);
+		this.settings.put("ACCELERATED_64_PROCESSOR_NUMBER", 10f);
+		this.settings.put("SPECIALIZED_NUMBER", 20f);
+		this.settings.put("SMALL_PRICE", 0.05f);
+		this.settings.put("MEDIUM_PRICE", 0.05f);
+		this.settings.put("LARGE_PRICE", 0.05f);
+		this.settings.put("HUGE_PRICE", 0.05f);
+		this.settings.put("COMPUTER_COST", 0.01f);
+
+		/**STUDENT_NUMBER;40;
+RESEARCHER NUMBER;20;
+COMPUTER_BUDGET;20;
+WATER_BUDGET;10;
+SOIL_BUDGET;5;
+NODES_16_PROCESSOR_NUMBER;80;
+NODES_32_PROCESSOR_NUMBER;40;
+NODES_64_PROCESSOR_NUMBER;30;
+ACCELERATED_16_PROCESSOR_NUMBER;30;
+ACCELERATED_32_PROCESSOR_NUMBER;20;
+ACCELERATED_64_PROCESSOR_NUMBER;10;
+SPECIALIZED_NUMBER;20;
+SMALL_PRICE;0.05;
+MEDIUM_PRICE;0.05;
+LARGE_PRICE;0.05;
+HUGE_PRICE;0.05;
+COMPUTER_COST;0.01;*/
+
 		return this.getSettings();
 	}
 
